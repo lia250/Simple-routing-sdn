e0f99c7fda11fdd182779d63a21b3e5c70f46748
lia250 <ali.github.79@gmail.com>
2025-06-03 20:41:54 +0330

    Update simple_sdn.py

--- mininet-topologies/simple_sdn.py
+++ mininet-topologies/simple_sdn.py
@@ -1,1 +1,43 @@
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
 
+def simple_sdn_network():
+    "Create a simple SDN network with 3 switches and 3 hosts"
+    
+    net = Mininet(controller=RemoteController)
+    
+    info('*** Adding controller\n')
+    c0 = net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6633)
+    
+    info('*** Adding hosts\n')
+    h1 = net.addHost('h1', ip='10.0.0.1')
+    h2 = net.addHost('h2', ip='10.0.0.2')
+    h3 = net.addHost('h3', ip='10.0.0.3')
+    
+    info('*** Adding switches\n')
+    s1 = net.addSwitch('s1')
+    s2 = net.addSwitch('s2')
+    s3 = net.addSwitch('s3')
+    
+    info('*** Creating links\n')
+    net.addLink(h1, s1)
+    net.addLink(h2, s2)
+    net.addLink(h3, s3)
+    net.addLink(s1, s2)
+    net.addLink(s2, s3)
+    net.addLink(s3, s1)
+    
+    info('*** Starting network\n')
+    net.start()
+    
+    info('*** Running CLI\n')
+    CLI(net)
+    
+    info('*** Stopping network')
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    simple_sdn_network()
